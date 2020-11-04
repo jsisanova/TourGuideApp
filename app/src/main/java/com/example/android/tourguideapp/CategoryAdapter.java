dd02d8b28fae6eec0739db1c738cf41e68a52e5f
jsisanova <jana.sisanova@gmail.com>
2020-11-04 22:42:33 +0100

    Do some small corrections

--- app/src/main/java/com/example/android/tourguideapp/CategoryAdapter.java
+++ app/src/main/java/com/example/android/tourguideapp/CategoryAdapter.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
  * each list item based on a data source which is a list of {@link Attraction} objects.
@@ -14,6 +18,10 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     /** Context of the app */
     // We need a Context object in order to turn the string resource ID into an actual String
     private Context mContext;
+
+    // Tab names
+    private String[] myPageListArray = new String[]{"Historical sites", "Events", "Parks", "Pubs"};
+
 
     /**
      * Create a new {@link CategoryAdapter} object.
@@ -56,16 +64,19 @@ public class CategoryAdapter extends FragmentPagerAdapter {
      */
     @Override
     public CharSequence getPageTitle(int position) {
-        switch (position) {
-            case 0:
-                return mContext.getString(R.string.category_historical_sites);
-            case 1:
-                return mContext.getString(R.string.category_events);
-            case 2:
-                return mContext.getString(R.string.category_parks);
-            default:
-                return  mContext.getString(R.string.category_pubs);
-        }
+
+        return myPageListArray[position];
+
+//        switch (position) {
+//            case 0:
+//                return mContext.getString(R.string.category_historical_sites);
+//            case 1:
+//                return mContext.getString(R.string.category_events);
+//            case 2:
+//                return mContext.getString(R.string.category_parks);
+//            default:
+//                return  mContext.getString(R.string.category_pubs);
+//        }
     }
 }
 
